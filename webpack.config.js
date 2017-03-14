09788cb5da41088ca570422d2ba4492c66c40001
JMLwen <1015521281@qq.com>
2017-03-14 10:33:19 +0800

    Revert "Revert "webpack.config.js""
    This reverts commit f3560ea5df6ee3a64962305d335c30876b44be94.

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+var webpack = require('webpack');
+var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    //页面入口文件配置
+    entry: {
+        name: [
+            'eventsource-polyfill',
+            './src/js/HiveData_router.jsx',
+        ]
+    },
+    //入口文件输出配置
+    output: {
+        path: './dist/',
+        filename: 'bundle.js',
+    },
+    //其他配置
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+    },
+    module: {
+        //加载器配置
+        loaders: [
+            { 
+                test: /\.css$/, 
+                loader: ExtractTextPlugin.extract("style-loader","css-loader") 
+            }, 
+
+            { 
+                test: /\.js$/, 
+                exclude: /node_modules/, 
+                loader: 'es3ify-loader'
+            },
+            {
+                test: /\.jsx$/,
+                loader: 'babel-loader!jsx-loader?harmony',            
+            }, 
+            {
+             test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/, 
+                loader: 'url-loader?limit=8192&name=./images/[hash:8].[name].[ext]'
+            }
+        ],
+        postLoaders: [
+          {
+            test: /\.js$/,
+            loaders: ['es3ify-loader']
+          }
+        ]
+    },
+    //插件项
+    plugins: [
+        commonsPlugin,
+        new ExtractTextPlugin("style.css"),
+    ]
+};
+
+
+
+
